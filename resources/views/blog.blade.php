be4f7380923b71704f9f8b5fea25229eaeac5b0e
canggihwr <canggihrinaldi8@gmail.com>
2022-05-30 19:15:26 +0800

    first up

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>C Blog | Blog Posts
+    </title>
+</head>
+<body>
+    <h1>
+        Halaman Blog Posts
+    </h1>
+    
+</body>
+</html>
